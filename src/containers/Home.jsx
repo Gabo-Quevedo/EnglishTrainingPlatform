258fe8726853df72127280ae0222ed56238a9da6
Gabriel Quevedo <gabriel.quevedo.c@gmail.com>
2020-11-03 02:23:55 -0300

    mend

---
+++ src/containers/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from '../components/Header';
+import NextTopic from '../components/NextTopic';
+import WeeklyChallenge from '../components/WeeklyChallenge';
+import Participants from '../components/Participants';
+import Dynamic from '../components/Dynamic';
+import Footer from '../components/Footer';
+import '../assets/styles/index.styl';
+
+const Home = () => (
+  <div className='App'>
+    <>
+      <Header />
+      <NextTopic />
+      <Dynamic />
+      <WeeklyChallenge />
+      <Participants />
+      <Footer />
+    </>
+  </div>
+
+);
+
+export default Home;
