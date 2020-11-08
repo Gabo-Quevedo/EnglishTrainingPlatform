10329edb67420a9ca1dd7763f983006076293673
Gabriel Quevedo <gabriel.quevedo.c@gmail.com>
2020-11-08 04:53:31 -0300

    Se emula login y redireccion con hooks

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
+import Login from '../containers/Login';
 import Layout from '../components/Layout';
 
 const App = () => (
@@ -8,7 +9,7 @@ const App = () => (
     <Layout>
       <Switch>
         <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Home} />
+        <Route exact path='/login' component={Login} />
         <Route component={Home} />
       </Switch>
     </Layout>
