609501a981884daf488c1c5d8844d59028ec277b
Gabriel Quevedo <gabriel.quevedo.c@gmail.com>
2020-11-08 07:06:15 -0300

    menu lateral de login estilizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import TrenIcon from '../assets/static/tren-icon.svg';
 import '../assets/styles/components/Header.styl';
 
 const Header = () => (
   <header className='header'>
-    <img src={TrenIcon} alt='logo-tren' />
     <div className='header--timeleft-container'>
+      <img src={TrenIcon} alt='logo-tren' />
       <h1 className='header--time-left'>
         <span>3</span>
         {' '}
@@ -16,6 +17,15 @@ const Header = () => (
         Hours
       </h1>
       <p>Next Meeting</p>
+    </div>
+    <div className='header__menu'>
+      <div className='header__menu--profile'>
+        <img src={TrenIcon} alt='' />
+      </div>
+      <ul>
+        <li><Link to='/'>Cuenta</Link></li>
+        <li><Link to='/'>Cerrar Sesión</Link></li>
+      </ul>
     </div>
   </header>
 );
