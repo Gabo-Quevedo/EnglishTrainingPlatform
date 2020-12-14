ea836167d98022a1ae5a9ee254cb71adbd556098
Gabriel Quevedo <gabriel.quevedo.c@gmail.com>
2020-12-14 05:54:14 -0300

    se finaliza maqueteo de My weekly challenges

--- src/components/ChallengeRequest.jsx
+++ src/components/ChallengeRequest.jsx
@@ -6,6 +6,32 @@ const ChallengeRequest = () => (
   <div className='challengeRequest'>
     <div className='challengeRequest-container'>
       <button type='button'>New Challenge Request</button>
+      <div className='challengeRequest-formBody'>
+        <form action=''>
+          <div className='challengeRequest-form-container'>
+            <label htmlFor='ch-title'>
+              Title
+              <input id='ch-title' type='text' />
+            </label>
+            <label htmlFor='ch-duration'>
+              Estimated Minutes Duration
+              <select name='ch-duration' id='ch-duration'>
+                <option value='0'>Select</option>
+                <option value='7'>7</option>
+                <option value='10'>10</option>
+                <option value='15'>15</option>
+                <option value='17'>17</option>
+                <option value=''>Special Ocasion</option>
+              </select>
+            </label>
+            <label htmlFor='ch-description'>
+              Description
+              <textarea id='ch-description' type='text' />
+            </label>
+            <input className='submit-button' type='submit' value='Send Request' />
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 );
