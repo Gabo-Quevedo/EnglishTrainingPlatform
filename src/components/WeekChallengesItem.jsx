5e0ea923517fd4d93daa5ad752e4c082639929de
Gabriel Quevedo <gabriel.quevedo.c@gmail.com>
2021-01-01 22:05:44 -0300

    Se estiliza challenge css

--- src/components/WeekChallengesItem.jsx
+++ src/components/WeekChallengesItem.jsx
@@ -7,11 +7,22 @@ import '../assets/styles/icons.styl';
 const WeekChallengesItem = ({ title, minutes, description }) => (
   <section className='week-challenges-item'>
     <div className='week-challenges-item-container'>
-      <h2>{title}</h2>
+      <div className='week-challenges-item-container__title'>
+        <h2>{title}</h2>
+        <div className='week-challenges-item-container__title-time'>
+          <span>
+            {minutes}
+          </span>
+          <div className='clock_icon' />
+        </div>
+      </div>
       <p>
         {description}
       </p>
-      <Link to='/'><div className='trash_icon' /></Link>
+      <div className='week-challenges-item-container__statusAction'>
+        <span>Done</span>
+        <Link to='/'><div className='trash_icon' /></Link>
+      </div>
     </div>
   </section>
 );
