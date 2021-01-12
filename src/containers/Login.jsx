7621dcf06853114913d7c0545d4670d9dab56c6b
Gabriel Quevedo <gabriel.quevedo.c@gmail.com>
2021-01-12 16:33:53 -0300

    Se reincorpora topics y Challenges a login de usuario para realizar pruebas consistentes

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -37,7 +37,53 @@ const Login = (props) => {
         'level': ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'],
       },
     ],
-    'topics': [],
+    'topics': [
+      {
+        'id': 1,
+        'title': 'Games',
+        'description': 'Games description',
+        'situation': {
+          'enable': 'true',
+          'request': {
+            'inQue': 'true',
+            'accepted': 'false',
+            'rejected': 'false',
+            'deleted': 'false',
+            'done': 'false',
+          },
+        },
+      },
+      {
+        'id': 2,
+        'title': 'Dance',
+        'description': 'Dance description',
+        'situation': {
+          'enable': 'true',
+          'request': {
+            'inQue': 'true',
+            'accepted': 'false',
+            'rejected': 'false',
+            'deleted': 'false',
+            'done': 'false',
+          },
+        },
+      },
+      {
+        'id': 3,
+        'title': 'Work',
+        'description': 'Work description',
+        'situation': {
+          'enable': 'true',
+          'request': {
+            'inQue': 'true',
+            'accepted': 'false',
+            'rejected': 'false',
+            'deleted': 'false',
+            'done': 'false',
+          },
+        },
+      },
+    ],
     'challenges': [
       {
         'id': 1,
